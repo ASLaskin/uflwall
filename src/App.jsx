fbe64e01f1851032df0fc4635fd39b672973b0b0
ASLaskin <andrewlaskin@ufl.edu>
2023-10-26 23:02:30 -0400

    add setup for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './header'
+
+
 function App() {
   return (
     <>
+      <Header />
       <h1 className='text-blue-500'>UFL Wall</h1>
     </>
   )

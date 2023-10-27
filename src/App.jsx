e89ec8b7892bbd97f652359508534a944f37ed10
ASLaskin <andrewlaskin@ufl.edu>
2023-10-27 13:46:07 -0400

    reformat

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './layout';
-import Home from './page';
+import Home from './pages/page';
 import NotFound from './pages/notfound'; 
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 

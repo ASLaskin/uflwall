ae89ebd9a8d97c59fcad3d920cd52cff178fa276
ASLaskin <andrewlaskin@ufl.edu>
2023-12-20 15:57:50 -0500

    no entries found

--- src/components/entries.jsx
+++ src/components/entries.jsx
@@ -8,9 +8,10 @@ import HashLoader from "react-spinners/HashLoader";
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
-const Entries = ({month,year,searchClicked}) => {
+const Entries = ({ month, year, searchClicked }) => {
   const [entries, setEntries] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [entriesExist, setEntriesExist] = useState(true);
 
   //They way this works is that it fetches all the entries from the database and then displays them on the page
   //The database is storing them as year -> month -> entries -> entry
@@ -18,12 +19,12 @@ const Entries = ({month,year,searchClicked}) => {
     const fetchEntries = async () => {
       try {
         const entriesData = [];
-
+  
         // Use month and year only if searchClicked is true
         if (searchClicked) {
           const entriesCollectionRef = collection(db, year, month, "entries");
           const entriesSnapshot = await getDocs(entriesCollectionRef);
-
+  
           entriesSnapshot.forEach((entryDoc) => {
             entriesData.push({
               id: entryDoc.id,
@@ -33,12 +34,15 @@ const Entries = ({month,year,searchClicked}) => {
           });
         } else {
           // Fetch all entries if search button is not clicked
-          const monthsList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-
+          const monthsList = [
+            "01", "02", "03", "04", "05", "06",
+            "07", "08", "09", "10", "11", "12",
+          ];
+  
           for (const currentMonth of monthsList) {
             const entriesCollectionRef = collection(db, "2023", currentMonth, "entries");
             const entriesSnapshot = await getDocs(entriesCollectionRef);
-
+  
             entriesSnapshot.forEach((entryDoc) => {
               entriesData.push({
                 id: entryDoc.id,
@@ -48,14 +52,17 @@ const Entries = ({month,year,searchClicked}) => {
             });
           }
         }
-
+  
         setEntries(entriesData);
         setLoading(false);
+  
+        // Check if entriesData has any entries
+        setEntriesExist(entriesData.length > 0);
       } catch (error) {
         console.error("Error fetching entries:", error);
       }
     };
-
+  
     fetchEntries();
   }, [db, month, year, searchClicked]);
 
@@ -76,17 +83,30 @@ const Entries = ({month,year,searchClicked}) => {
             </h2>
           </div>
         ) : (
-          <ul className="list-disc pl-6">
-            {entries.map((entry) => (
-              <li
-                key={entry.id}
-                className="text-black flex flex-row space-x-2 mb-2"
-              >
-                <p className="text-lg">{entry.note}</p>
-                <p className="text-gray-500">- {entry.timestamp}</p>
-              </li>
-            ))}
-          </ul>
+          <>
+            {entriesExist ? (
+              <ul className="list-disc pl-6">
+                {entries.map((entry) => (
+                  <li
+                    key={entry.id}
+                    className="text-black flex flex-row space-x-2 mb-2"
+                  >
+                    <p className="text-lg">{entry.note}</p>
+                    <p className="text-gray-500">- {entry.timestamp}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="flex items-center justify-center flex-col ">
+                <h1 className="text-3xl font-bold text-customBlue">
+                  No entries found
+                </h1>
+                <h2 className="text-2xl font-bold text-customBlue">
+                  Try searching for a different month
+                </h2>
+              </div>
+            )}
+          </>
         )}
       </div>
     </div>
